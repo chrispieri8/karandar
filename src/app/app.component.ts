21b9aa25704ead6c83bf77504954683ab212dd53
Chris Pieri <chrispieri3@gmail.com>
2023-10-18 20:53:53 -0400

    Add calendar package

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'karandar';
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin],
+    eventClick: this.handleDateClick.bind(this), // MUST ensure `this` context is maintained
+    events: [
+      { title: 'event 1', date: '2019-04-01' },
+      { title: 'event 2', date: '2019-04-02' },
+    ],
+    themeSystem: 'journal',
+  };
+
+  handleDateClick(arg: any) {
+    alert('date click! ' + arg.dateStr);
+  }
 }
